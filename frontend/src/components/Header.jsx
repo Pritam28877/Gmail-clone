b01f71dd46fc294db8c96c3551569af95444819f
Pritam Das <dpritam28577@gmail.com>
2023-10-28 11:34:52 +0530

    add the gmail icon and the Menubar css

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,14 +1,25 @@
 import React from "react";
-import { AppBar, Toolbar } from "@mui/material";
+import { AppBar, Toolbar, styled } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { gmailLogo } from "../constants/constant";
 
+const StyledAppBar = styled(AppBar)({
+  background: "#F5F5F5",
+  boxShadow: "none",
+});
 const Header = () => {
   return (
     <>
-      <AppBar position="static">
+      <StyledAppBar position="static">
         <Toolbar>
-                                
+          <MenuIcon color="action" />
+          <img
+            src={gmailLogo}
+            alt="gmail logo"
+            style={{ width: 110, marginLeft: 20 }}
+          />
         </Toolbar>
-      </AppBar>
+      </StyledAppBar>
     </>
   );
 };
