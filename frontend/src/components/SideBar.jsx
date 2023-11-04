bedbb69ef68a06142aedae714f1eb4f7f6fce993
Pritam Das <dpritam28577@gmail.com>
2023-11-04 14:16:07 +0530

    Add persistent sidebar with Drawer component

--- frontend/src/components/SideBar.jsx
+++ frontend/src/components/SideBar.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Drawer, styled } from "@mui/material";
 
 const SideBar = () => {
   return (
-    <div>SideBar</div>
-  )
-}
+    <Drawer
+      anchor="left"
+      open={true}
+      hideBackdrop={true}
+      ModalProps={{ keepMounted: true }}
+      variant="persistent"
+      sx={{
+        width: "240px",
+        flexShrink: 0,
+        "& .MuiDrawer-paper": {
+          marginTop: "64px",
+          width: "240px",
+          boxSizing: "border-box",
+          background: "#F5F5F5",
+          borderRight: "none",
+        },
+      }}
+    >
+      hello
+    </Drawer>
+  );
+};
 
-export default SideBar
+export default SideBar;
