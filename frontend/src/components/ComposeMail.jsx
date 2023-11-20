79f4465555d04d89edbfc871f684b178cfda606f
Pritam Das <dpritam28577@gmail.com>
2023-11-20 13:52:25 +0530

    Update ComposeMail component to use custom dialog style

--- frontend/src/components/ComposeMail.jsx
+++ frontend/src/components/ComposeMail.jsx
@@ -2,9 +2,19 @@ import React from "react";
 import { Dialog } from "@mui/material";
 
 const ComposeMail = () => {
+  const dialogStyle = {
+    height: "90%",
+    width: "80%",
+    maxWidth: "100%",
+    maxHeight: "100%",
+    boxShadow: "none",
+    border: " 10px 10px 0 0",
+  };
   return (
     <>
-      <Dialog>ComposeMail</Dialog>
+      <Dialog open={true} PaperProps={{ sx: dialogStyle }}>
+        ComposeMail
+      </Dialog>
     </>
   );
 };
