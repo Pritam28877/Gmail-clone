3e1a3736daef01484641b8ee775150c45fb9ca4b
Pritam Das <dpritam28577@gmail.com>
2023-11-20 13:11:13 +0530

    Add list items to SideBarContent component

--- frontend/src/components/SideBarContent.jsx
+++ frontend/src/components/SideBarContent.jsx
@@ -1,6 +1,7 @@
 import React from "react";
-import { Box, Button, styled } from "@mui/material";
+import { Box, Button, List, ListItem, styled } from "@mui/material";
 import CreateOutlinedIcon from "@mui/icons-material/CreateOutlined";
+import { SIDEBAR_DATA } from "../config/sidebar.config";
 
 const ComposedButton = styled(Button)({
   background: "#c2e7ff",
@@ -8,20 +9,25 @@ const ComposedButton = styled(Button)({
   padding: 15,
   borderRadius: 16,
   minWidth: 145,
-  textTransform:"none",
+  textTransform: "none",
 });
 
 const SideBarContent = () => {
   return (
     <>
       <Box>
-        <Box>
-          <ComposedButton>
-            <CreateOutlinedIcon />
-            Compose
-          </ComposedButton>
-        </Box>
-        <Box></Box>
+        <ComposedButton>
+          <CreateOutlinedIcon />
+          Compose
+        </ComposedButton>
+        <List>
+          {SIDEBAR_DATA.map((data) => (
+            <ListItem>
+              <data.icon />
+              {data.title}
+            </ListItem>
+          ))}
+        </List>
       </Box>
     </>
   );
--- frontend/src/config/sidebar.config.js
+++ frontend/src/config/sidebar.config.js
@@ -1,4 +1,4 @@
-import { routes } from "../routes/routes";
+// import { routes } from "../routes/routes";
 import {
   Photo,
   StarOutline,
@@ -13,36 +13,36 @@ export const SIDEBAR_DATA = [
     name: "inbox",
     title: "Inbox",
     icon: Photo,
-    path: routes.emails.path,
+    // path: routes.emails.path,
   },
   {
     name: "starred",
     title: "Starred",
     icon: StarOutline,
-    path: routes.emails.path,
+    // path: routes.emails.path,
   },
   {
     name: "sent",
     title: "Sent",
     icon: SendOutlined,
-    path: routes.emails.path,
+    // path: routes.emails.path,
   },
   {
     name: "drafts",
     title: "Drafts",
     icon: InsertDriveFileOutlined,
-    path: routes.emails.path,
+    // path: routes.emails.path,
   },
   {
     name: "bin",
     title: "Bin",
     icon: DeleteOutlined,
-    path: routes.emails.path,
+    // path: routes.emails.path,
   },
   {
     name: "allmail",
     title: "All Mail",
     icon: MailOutlined,
-    path: routes.emails.path,
+    // path: routes.emails.path,
   },
 ];
