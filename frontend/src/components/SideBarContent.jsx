89b01a25f452b1cb9dc1ec1927f54f6ff09d9bb4
Pritam Das <dpritam28577@gmail.com>
2023-11-20 13:02:50 +0530

    Add styled button for composing messages

--- frontend/src/components/SideBarContent.jsx
+++ frontend/src/components/SideBarContent.jsx
@@ -1,12 +1,25 @@
 import React from "react";
-import { Box, Button } from "@mui/material";
+import { Box, Button, styled } from "@mui/material";
+import CreateOutlinedIcon from "@mui/icons-material/CreateOutlined";
+
+const ComposedButton = styled(Button)({
+  background: "#c2e7ff",
+  color: "#001d35",
+  padding: 15,
+  borderRadius: 16,
+  minWidth: 145,
+  textTransform:"none",
+});
 
 const SideBarContent = () => {
   return (
     <>
       <Box>
         <Box>
-          <Button>Compose</Button>
+          <ComposedButton>
+            <CreateOutlinedIcon />
+            Compose
+          </ComposedButton>
         </Box>
         <Box></Box>
       </Box>
