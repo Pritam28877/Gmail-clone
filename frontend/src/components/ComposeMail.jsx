c4d1cde71dce8ded0680f9f389586fd9526264c6
Pritam Das <dpritam28577@gmail.com>
2023-11-26 15:56:03 +0530

    Add recipients and subject input fields to ComposeMail component

--- frontend/src/components/ComposeMail.jsx
+++ frontend/src/components/ComposeMail.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Dialog, Box, Typography, styled } from "@mui/material";
+import { Dialog, Box, Typography, styled, InputBase } from "@mui/material";
 import { Close, DeleteOutline } from "@mui/icons-material";
 
 const ComposeMail = () => {
@@ -21,14 +21,38 @@ const ComposeMail = () => {
       font-weight: 500;
     }
   `;
+  const RecipientsWrapper = styled(Box)({
+    display: "flex",
+    flexDirection: "column",
+    padding: "10px 15px",
+    "& > div": {
+      fontSize: "14px",
+      display: "flex",
+      alignItems: "center",
+      padding: "5px 0",
+      "& > input": {
+        marginLeft: "10px",
+        border: "none",
+        outline: "none",
+        fontSize: "14px",
+        fontWeight: 500,
+        "&::placeholder": {
+          color: "#b1b1b1",
+        },
+      },
+    },
+  });
   return (
     <>
       <Dialog open={true} PaperProps={{ sx: dialogStyle }}>
-         <Header>
-                <Typography>New Message</Typography>
-                <Close fontSize="small"  />
-            </Header>
-        <Box></Box>
+        <Header>
+          <Typography>New Message</Typography>
+          <Close fontSize="small" />
+        </Header>
+        <RecipientsWrapper>
+          <InputBase placeholder="Recipients" />
+          <InputBase placeholder="Subject" />
+        </RecipientsWrapper>
         <Box>TextAraa</Box>
         <Box></Box>
       </Dialog>
